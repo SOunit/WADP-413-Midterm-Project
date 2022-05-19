4430ac2097f14573c4f63d421e4a2498f1a1ba2e
SOunit <workbookwork7@gmail.com>
2022-05-19 16:45:46 -0700

    add create screen

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express from "express";
 import path from "path";
 import routes from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const BLOG_DATA_LIST = [
+  { _id: "1", title: "title 1" },
+  { _id: "2", title: "title 2" },
+  { _id: "3", title: "title 3" },
+];
+
 app.use((req, res, next) => {
-  (req as any).BLOG_DATA_LIST = [
-    { _id: "1", title: "1" },
-    { _id: "2", title: "2" },
-    { _id: "3", title: "3" },
-  ];
+  (req as any).blogList = BLOG_DATA_LIST;
 
   next();
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // FIXME: set proper type to request object
 router.get("/", (req, res) => {
-  res.render("list", { blogList: (req as any).BLOG_DATA_LIST });
+  res.render("list", { blogList: (req as any).blogList });
 });
 
 router.get("/login", (req, res) => {
@@ -12,6 +12,18 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
+  res.redirect("/");
+});
+
+router.get("/create", (req, res) => {
+  res.render("create", { blog: {} });
+});
+
+router.post("/create", (req, res) => {
+  const { title } = req.body;
+
+  (req as any).blogList.push({ id: Math.random(), title });
+
   res.redirect("/");
 });
 
