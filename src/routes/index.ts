3923c2f4dd09c06a2be68b9cbfa9675d82e24651
SOunit <workbookwork7@gmail.com>
2022-05-19 16:52:29 -0700

    add edit logic

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -27,4 +27,20 @@ router.post("/create", (req, res) => {
   res.redirect("/");
 });
 
+router.get("/edit/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  res.render("edit", { blog: (req as any).blogList[0] });
+});
+
+router.post("/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  (req as any).blogList[0].title = title;
+
+  res.redirect("/");
+});
+
 export default router;
